61d1876fa941dbbe88a1cee20b0e3711b7c85798
yiplee <guoyinl@gmail.com>
2017-09-13 16:55:11 +0800

    workaroud for fix call decisionHandler mutiple times

--- WebViewJavascriptBridge/WKWebViewJavascriptBridge.m
+++ WebViewJavascriptBridge/WKWebViewJavascriptBridge.m
@@ -147,6 +147,8 @@
             [_base logUnkownMessage:url];
         }
         decisionHandler(WKNavigationActionPolicyCancel);
+        
+        return;
     }
     
     if (strongDelegate && [strongDelegate respondsToSelector:@selector(webView:decidePolicyForNavigationAction:decisionHandler:)]) {
